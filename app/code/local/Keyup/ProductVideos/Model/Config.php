bd3dc83e49a3688f23cf3f5e606bf19a6668a26c
Joost Rohde <j.rohde@bd8.nl>
2016-03-01 18:50:11 +0100

    Added responsive sizing

--- app/code/local/Keyup/ProductVideos/Model/Config.php
+++ app/code/local/Keyup/ProductVideos/Model/Config.php
@@ -13,39 +13,26 @@ class Keyup_ProductVideos_Model_Config extends Mage_Core_Model_Abstract {
     const   PICTURE_FIRST = 'PV_PM',
             MOVIES_FISRT = 'PV_MP',
             MOVIES_UNDER_DESCRIPTION = 'PV_DES';
-    const   XML_PATH_VIDEO_BEHAVIOR = 'design/productvideos/pvbehavior',
-            XML_PATH_VIDEO_WIDTH = 'design/productvideos/pvwidth',
-            XML_PATH_VIDEO_HEIGHT = 'design/productvideos/pvheight';
+    const   XML_PATH_VIDEO_BEHAVIOR = 'design/productvideos/pvbehavior';
     const   ATTR_URLS = 'productvideos_ta';
-    
+
     private static function getVideoBehavior($storeId = null)
     {
         return Mage::getStoreConfig(self::XML_PATH_VIDEO_BEHAVIOR, $storeId);
     }
-    
+
     function isBehaviorPicturesFirst($storeId = null)
     {
         return (self::getVideoBehavior($storeId) == self::PICTURE_FIRST) ? true : false;
     }
-    
+
     function isBehaviorMoviesFirst($storeId = null)
     {
         return (self::getVideoBehavior($storeId) == self::MOVIES_FISRT) ? true : false;
     }
-    
+
     function isBehaviorMoviesUnderDescription($storeId = null)
     {
         return (self::getVideoBehavior($storeId) == self::MOVIES_UNDER_DESCRIPTION) ? true : false;
     }
-    
-    function getVideoWidth($storeId = null)
-    {
-        return Mage::getStoreConfig(Keyup_ProductVideos_Model_Config::XML_PATH_VIDEO_WIDTH, $storeId);
-    }
-    
-    function getVideoHeight($storeId = null)
-    {
-        return Mage::getStoreConfig(Keyup_ProductVideos_Model_Config::XML_PATH_VIDEO_HEIGHT, $storeId);
-    }
-    
 }
