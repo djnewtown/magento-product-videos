58a827f7f76859a29201026ccdd8d1457f86465c
Joost Rohde <j.rohde@bd8.nl>
2016-03-01 18:57:42 +0100

    Added start/stop on video click

--- skin/frontend/base/default/js/keyup/productvideos.js
+++ skin/frontend/base/default/js/keyup/productvideos.js
@@ -2,14 +2,27 @@ document.observe('dom:loaded', function(){
     $$('div.more-views').first().observe('click', function (e) {
         var clickedThumb = e.findElement('.thumb-link');
         if (clickedThumb) {
+            stopVideo();
             $$('.productvideos-gallery').invoke('show').invoke('removeClassName', 'productvideos-hidden');
             $$('.productvideos-player').invoke('hide');
         }
         var clickedThumbV = e.findElement('.thumb-link-pv');
         if (clickedThumbV) {
             e.stop();
+            stopVideo();
+            var videoId = clickedThumbV.readAttribute('data-video-index');
             $$('.productvideos-gallery, .productvideos-player').invoke('hide').invoke('removeClassName', 'productvideos-hidden');
-            $$('.productvideos-player-'+clickedThumbV.readAttribute('data-video-index')).invoke('show');
+            $$('.productvideos-player-'+videoId).invoke('show');
+            var url = $$('.productvideos-player-'+videoId+' > iframe')[0].src;
+            $$('.productvideos-player-'+videoId+' > iframe')[0].src = url+"&autoplay=1";
         }
     });
 });
+
+stopVideo = function() {
+    $$('.productvideos-player iframe').each(function(i) {
+        var url = i.src;
+        i.src = '';
+        i.src = url.replace("&autoplay=1", "");
+    });
+}
