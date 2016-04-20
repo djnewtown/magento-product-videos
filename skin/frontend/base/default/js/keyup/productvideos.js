3cde26133b8adb8c649e30f1ae03188932500bcd
Joost Rohde <j.rohde@bd8.nl>
2016-04-20 14:47:21 +0200

    Hide zoomcontainer on video display

--- skin/frontend/base/default/js/keyup/productvideos.js
+++ skin/frontend/base/default/js/keyup/productvideos.js
@@ -1,22 +1,26 @@
 document.observe('dom:loaded', function(){
-    $$('div.more-views').first().observe('click', function (e) {
-        var clickedThumb = e.findElement('.thumb-link');
-        if (clickedThumb) {
-            stopVideo();
-            $$('.productvideos-gallery').invoke('show').invoke('removeClassName', 'productvideos-hidden');
-            $$('.productvideos-player').invoke('hide');
-        }
-        var clickedThumbV = e.findElement('.thumb-link-pv');
-        if (clickedThumbV) {
-            e.stop();
-            stopVideo();
-            var videoId = clickedThumbV.readAttribute('data-video-index');
-            $$('.productvideos-gallery, .productvideos-player').invoke('hide').invoke('removeClassName', 'productvideos-hidden');
-            $$('.productvideos-player-'+videoId).invoke('show');
-            var url = $$('.productvideos-player-'+videoId+' > iframe')[0].src;
-            $$('.productvideos-player-'+videoId+' > iframe')[0].src = url+"&autoplay=1";
-        }
-    });
+    if($$('div.more-views').first() != undefined) {
+        $$('div.more-views').first().observe('click', function (e) {
+            var clickedThumb = e.findElement('.thumb-link');
+            if (clickedThumb) {
+                stopVideo();
+                $$('.zoomContainer').invoke('show');
+                $$('.productvideos-gallery').invoke('show').invoke('removeClassName', 'productvideos-hidden');
+                $$('.productvideos-player').invoke('hide');
+            }
+            var clickedThumbV = e.findElement('.thumb-link-pv');
+            if (clickedThumbV) {
+                e.stop();
+                stopVideo();
+                $$('.zoomContainer').invoke('hide');
+                var videoId = clickedThumbV.readAttribute('data-video-index');
+                $$('.productvideos-gallery, .productvideos-player').invoke('hide').invoke('removeClassName', 'productvideos-hidden');
+                $$('.productvideos-player-'+videoId).invoke('show');
+                var url = $$('.productvideos-player-'+videoId+' > iframe')[0].src;
+                $$('.productvideos-player-'+videoId+' > iframe')[0].src = url+"&autoplay=1";
+            }
+        });
+    };
 });
 
 stopVideo = function() {
